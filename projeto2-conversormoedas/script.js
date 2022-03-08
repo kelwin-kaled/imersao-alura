bf902f5b5e6b6d02511588ac94ff4c19a2ea90d3
kelwin-kaled <101134615+kelwin-kaled@users.noreply.github.com>
2022-03-08 10:07:31 -0300

    atualização de projeto

--- projeto2-conversormoedas/script.js
+++ projeto2-conversormoedas/script.js
@@ -1,3 +1,11 @@
 function converter(){
-    
+    let num = document.getElementById('num')
+    let res = document.getElementById('res')
+    if(num.value==''){
+        window.alert('Digite um valor válido')
+    }else{
+        num = Number(num.value)
+        res.innerHTML = `O valor em dolares é: U$${(5.09*num).toFixed(2)}`
+    }
+
 }
